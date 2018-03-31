453a73ef771e8516430be3f15738d8ba70fcd4ee
susmitharajan <susmithasundararajan6@gmail.com>
2018-03-31 10:44:25 +0530

    Navigation Drawer added

--- app/src/main/java/com/example/susmitharajan/musicplayeroriginal/MainActivity.java
+++ app/src/main/java/com/example/susmitharajan/musicplayeroriginal/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -11,7 +13,8 @@ import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
     FragmentManager FM;
     FragmentTransaction FT;
 
@@ -21,9 +24,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        navigationView= (NavigationView) findViewById(R.id.shitstuff);
         FM= getSupportFragmentManager();
         FT= FM.beginTransaction();
         FT.replace(R.id.containerView, new TabFragment()).commit();
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                drawerLayout.closeDrawers();
+
+                if (item.getItemId()== R.id.nav_item_inbox) {
+                    FragmentTransaction fragmentTransaction= FM.beginTransaction();
+                    fragmentTransaction.replace(R.id.containerView, new EqualizerFragment()).commit();
+                }
+
+                if (item.getItemId()==R.id.nav_item_sent)
+                {
+                    FragmentTransaction fragmentTransaction1=FM.beginTransaction();
+                    fragmentTransaction1.replace(R.id.containerView,new TabFragment()).commit();
+
+                }
+                return false;
+            }
+        });
+
+
+        android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        ActionBarDrawerToggle toggle= new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.app_name,R.string.app_name);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
 
     }
 }
